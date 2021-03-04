a74caf7c4e08efd6922a7cbd7ab0397b840cad9d
Skoognuts <77204481+Skoognuts@users.noreply.github.com>
2021-03-04 16:02:56 +0100

    Add files via upload

--- app_installer.py
+++ app_installer.py
@@ -6,21 +6,65 @@ import sqlite3
 import logging
 
 from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QDesktopWidget, QMessageBox
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QToolTip, QMessageBox, QLabel)
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-
-from api_mat_v_2 import *
 
 CUR_DIR = os.path.dirname(__file__)
 DATA_DIR = os.path.join(CUR_DIR, "data")
 ICON_FILE = os.path.join(DATA_DIR, "icone.png")
-LOG_FILE = os.path.join(CUR_DIR, "log/mat_app_installer.log")
+REX_FILE = os.path.join(DATA_DIR, "installer_rex.png")
+LOG_FILE = os.path.join(CUR_DIR, "log/mat_app.log")
 
-logging.basicConfig(level = logging.DEBUG, filename = LOG_FILE, filemode = "w", format = "%(levelname)s - %(message)s")
-logging.debug("MATOSAURUS INSTALLER LAUNCHED")
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("MATOSAURUS SUITE INSTALLER - Alpha")
+        self.setStyleSheet("background-color: rgb(200, 200, 255)")
+        self.setMinimumWidth(750)
+        self.setMinimumHeight(508)
+        self.setMaximumWidth(750)
+        self.setMaximumHeight(508)
+        self.setWindowIcon(QtGui.QIcon(ICON_FILE))
+        self.windowUI()
+        self.setup_connections()
+        self.show()
 
-## Créer l'interface utilisateur.
+    def windowUI(self):
+        self.grid_layout = QtWidgets.QGridLayout(self)
+
+        self.lbl_rex_pic = QtWidgets.QLabel(self)
+        pixmap = QPixmap(REX_FILE)
+        self.lbl_rex_pic.setPixmap(pixmap)
+        self.lbl_rex_pic.setMinimumWidth(246)
+        self.lbl_rex_pic.setMinimumHeight(508)
+
+        self.btn_continue = QPushButton("Continuer", self)
+
+        self.grid_layout.addWidget(self.lbl_rex_pic, 0, 0, 1, 1)
+        self.grid_layout.addWidget(self.btn_continue, 0, 1, 1, 1)
+
+        
+
+    def setup_connections(self):
+        self.btn_continue.clicked.connect(self.window2)
+
+    def window2(self):
+        self.w = Window2()
+        self.w.show()
+        self.hide()
+
+class Window2(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("MATOSAURUS SUITE INSTALLER - Alpha")
+        self.setStyleSheet("background-color: rgb(200, 200, 255)")
+        self.setWindowIcon(QtGui.QIcon(ICON_FILE))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    sys.exit(app.exec())
 
 ## Choisir la version à installer.
 
