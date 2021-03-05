0d31660182398184c40661b95668c4708c099511
Skoognuts <77204481+Skoognuts@users.noreply.github.com>
2021-03-05 09:25:46 +0100

    Add files via upload

--- app_installer.py
+++ app_installer.py
@@ -34,8 +34,8 @@ class Window(QtWidgets.QWidget):
     def windowUI(self):
         self.grid_layout = QtWidgets.QGridLayout(self)
 
-        self.lbl_desc_1 = QtWidgets.QLabel("Vous êtes sur le point d'installer la suite MATOSAURUS sur votre ordinateur.")
-        self.lbl_desc_2 = QtWidgets.QLabel("Avant de démarrer l'installation, il est recommandé de fermer toutes les \nautres applications. Cela permettra la mise à jour de certains fichiers système \nsans redémarrer votre ordinateur.")
+        self.lbl_desc_1 = QtWidgets.QLabel("Vous êtes sur le point d'installer la suite MATOSAURUS sur votre \nordinateur.")
+        self.lbl_desc_2 = QtWidgets.QLabel("Avant de démarrer l'installation, il est recommandé de fermer toutes les \nautres applications. Cela permettra la mise à jour de certains fichiers \nsystème sans redémarrer votre ordinateur.")
         self.lbl_desc_3 = QtWidgets.QLabel("Cliquez sur Suivant pour continuer.")
 
         self.lbl_desc_1.setFont(QtGui.QFont("sanserif", 10))
@@ -46,7 +46,11 @@ class Window(QtWidgets.QWidget):
         self.lbl_title.setFont(QtGui.QFont("sanserif", 18))
         self.lbl_title.setStyleSheet('color:black')
 
-        self.btn_continue = QtWidgets.QPushButton("Suivant  >", self)
+        self.btn_continue_1 = QtWidgets.QPushButton("Suivant  >", self)
+        self.btn_cancel_1 = QtWidgets.QPushButton("Annuler", self)
+
+        self.btn_continue_1.setStyleSheet("background-color: rgb(150, 150, 255)")
+        self.btn_cancel_1.setStyleSheet("background-color: rgb(150, 150, 255)")
 
         self.lbl_rex_pic = QtWidgets.QLabel(self)
         pixmap = QPixmap(REX_FILE)
@@ -57,10 +61,11 @@ class Window(QtWidgets.QWidget):
         self.grid_layout.addWidget(self.lbl_desc_1, 1, 1, 1, 4)
         self.grid_layout.addWidget(self.lbl_desc_2, 2, 1, 1, 4)
         self.grid_layout.addWidget(self.lbl_desc_3, 3, 1, 1, 4)
-        self.grid_layout.addWidget(self.btn_continue, 5, 4, 1, 1)
+        self.grid_layout.addWidget(self.btn_continue_1, 5, 3, 1, 1)
+        self.grid_layout.addWidget(self.btn_cancel_1, 5, 4, 1, 1)
 
     def setup_connections(self):
-        self.btn_continue.clicked.connect(self.window2)
+        self.btn_continue_1.clicked.connect(self.window2)
 
     def window2(self):
         self.w = Window2()
@@ -72,9 +77,9 @@ class Window2(QMainWindow):
         super().__init__()
         self.setWindowTitle("MATOSAURUS SUITE INSTALLER - Alpha")
         self.setStyleSheet("background-color: rgb(200, 200, 255)")
-        self.setMinimumWidth(750)
+        self.setMinimumWidth(700)
         self.setMinimumHeight(508)
-        self.setMaximumWidth(750)
+        self.setMaximumWidth(700)
         self.setMaximumHeight(508)
         self.setWindowIcon(QtGui.QIcon(ICON_FILE))
 
