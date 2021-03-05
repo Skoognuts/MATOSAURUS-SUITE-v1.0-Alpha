dda833cf1b495600c075295489edadbcd7ea062a
Skoognuts <77204481+Skoognuts@users.noreply.github.com>
2021-03-05 09:18:37 +0100

    Add files via upload

--- app_installer.py
+++ app_installer.py
@@ -21,9 +21,9 @@ class Window(QtWidgets.QWidget):
         super().__init__()
         self.setWindowTitle("MATOSAURUS SUITE INSTALLER - Alpha")
         self.setStyleSheet("background-color: rgb(200, 200, 255)")
-        self.setMinimumWidth(750)
+        self.setMinimumWidth(700)
         self.setMinimumHeight(508)
-        self.setMaximumWidth(750)
+        self.setMaximumWidth(700)
         self.setMaximumHeight(508)
         self.setWindowIcon(QtGui.QIcon(ICON_FILE))
         
@@ -34,19 +34,30 @@ class Window(QtWidgets.QWidget):
     def windowUI(self):
         self.grid_layout = QtWidgets.QGridLayout(self)
 
-        self.lbl_title = QtWidgets.QLabel("Bienvenue dans le programme d'installation \nde la suite MATOSAURUS.")
+        self.lbl_desc_1 = QtWidgets.QLabel("Vous êtes sur le point d'installer la suite MATOSAURUS sur votre ordinateur.")
+        self.lbl_desc_2 = QtWidgets.QLabel("Avant de démarrer l'installation, il est recommandé de fermer toutes les \nautres applications. Cela permettra la mise à jour de certains fichiers système \nsans redémarrer votre ordinateur.")
+        self.lbl_desc_3 = QtWidgets.QLabel("Cliquez sur Suivant pour continuer.")
+
+        self.lbl_desc_1.setFont(QtGui.QFont("sanserif", 10))
+        self.lbl_desc_2.setFont(QtGui.QFont("sanserif", 10))
+        self.lbl_desc_3.setFont(QtGui.QFont("sanserif", 10))
+
+        self.lbl_title = QtWidgets.QLabel("Bienvenue dans le programme \nd'installation de la suite MATOSAURUS.")
         self.lbl_title.setFont(QtGui.QFont("sanserif", 18))
         self.lbl_title.setStyleSheet('color:black')
 
-        self.btn_continue = QtWidgets.QPushButton("Continuer", self)
+        self.btn_continue = QtWidgets.QPushButton("Suivant  >", self)
 
         self.lbl_rex_pic = QtWidgets.QLabel(self)
         pixmap = QPixmap(REX_FILE)
         self.lbl_rex_pic.setPixmap(pixmap)
 
-        self.grid_layout.addWidget(self.lbl_rex_pic, 0, 0, 2, 1)
+        self.grid_layout.addWidget(self.lbl_rex_pic, 0, 0, 6, 1)
         self.grid_layout.addWidget(self.lbl_title, 0, 1, 1, 4)
-        self.grid_layout.addWidget(self.btn_continue, 1, 4, 1, 1)
+        self.grid_layout.addWidget(self.lbl_desc_1, 1, 1, 1, 4)
+        self.grid_layout.addWidget(self.lbl_desc_2, 2, 1, 1, 4)
+        self.grid_layout.addWidget(self.lbl_desc_3, 3, 1, 1, 4)
+        self.grid_layout.addWidget(self.btn_continue, 5, 4, 1, 1)
 
     def setup_connections(self):
         self.btn_continue.clicked.connect(self.window2)
