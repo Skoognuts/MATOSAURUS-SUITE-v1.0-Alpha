370a036c0044b3fd805711697e516f3974bbe299
Skoognuts <77204481+Skoognuts@users.noreply.github.com>
2021-03-15 16:13:34 +0100

    Add files via upload

--- app_installer.py
+++ app_installer.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import json
 import sqlite3
 import logging
 
@@ -12,13 +13,12 @@ from PyQt5.QtGui import *
 
 CUR_DIR = os.path.dirname(__file__)
 DATA_DIR = os.path.join(CUR_DIR, "data")
+VERS_FILE = os.path.join(DATA_DIR, "version.json")
+PATH_FILE = os.path.join(DATA_DIR, "path.json")
+SHARED_PATH_FILE = os.path.join(DATA_DIR, "shared_path.json")
 ICON_FILE = os.path.join(DATA_DIR, "icone.png")
 REX_FILE = os.path.join(DATA_DIR, "installer_rex.png")
 LOG_FILE = os.path.join(CUR_DIR, "log/mat_app.log")
-
-class Version(str):
-    def __init__(self, vers):
-        self.vers = ""
 
 class Window(QtWidgets.QWidget):
     def __init__(self):
@@ -172,12 +172,18 @@ class Window2(QtWidgets.QWidget):
     def window3(self):
         global version
         if self.cb_prog_1.isChecked() :
-            version = Version("Matosaurus Rex")
+            with open(VERS_FILE, "w", encoding='utf8') as f :
+                vers = {"version" : "Matosaurus Rex"}
+                json.dump(vers, f, ensure_ascii=False)
+            f.close()
             self.w = Window3()
             self.w.show()
             self.hide()
         elif self.cb_prog_2.isChecked() :
-            version = Version("Matosaurus Viewer")
+            with open(VERS_FILE, "w", encoding='utf8') as f :
+                vers = {"version" : "Matosaurus Viewer"}
+                json.dump(vers, f, ensure_ascii=False)
+            f.close()
             self.w = Window3()
             self.w.show()
             self.hide()
@@ -239,8 +245,11 @@ class Window3(QtWidgets.QWidget):
         global DISC
         global default_path
         DISC = CUR_DIR.split(":/")[0]
-        vers = Version(version)
-        default_path = f"{DISC.capitalize()}:\\{vers}"
+        with open(VERS_FILE, "r", encoding='utf8') as f :
+            data = json.load(f)
+        f.close()
+        vers = data["version"]
+        default_path = f"{DISC.capitalize()}:/{vers}"
         self.le_browse_3.setText(default_path)
 
     def setup_connections(self):
@@ -249,8 +258,7 @@ class Window3(QtWidgets.QWidget):
         self.btn_browse_3.clicked.connect(self.browser)
 
     def browser(self):
-        global path
-        root_path = f"{DISC.capitalize()}:\\"
+        root_path = f"{DISC.capitalize()}:/"
         path = QFileDialog.getExistingDirectory(self, "Sélectionner un dossier d'installation :", root_path)
         if path != default_path :
             self.le_browse_3.setText(path)
@@ -264,9 +272,185 @@ class Window3(QtWidgets.QWidget):
         self.close()
 
     def window4(self):
+        with open(PATH_FILE, "w", encoding='utf8') as f :
+            vers = {"directory" : self.le_browse_3.text()}
+            json.dump(vers, f, ensure_ascii=False)
+        f.close()
         self.w = Window4()
         self.w.show()
         self.close()
+
+class Window4(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("MATOSAURUS SUITE INSTALLER - Alpha")
+        self.setStyleSheet("background-color: rgb(215, 215, 255)")
+        self.setMinimumWidth(700)
+        self.setMinimumHeight(508)
+        self.setMaximumWidth(700)
+        self.setMaximumHeight(508)
+        self.setWindowIcon(QtGui.QIcon(ICON_FILE))
+        
+        self.windowUI()
+        self.setup_default()
+        self.setup_connections()
+        self.show()
+
+    def windowUI(self):
+        self.grid_layout = QtWidgets.QGridLayout(self)
+
+        self.lbl_title = QtWidgets.QLabel("Choisir le dossier de destination pour la \nbase de données partagée")
+        self.lbl_title.setFont(QtGui.QFont("sanserif", 18))
+        self.lbl_title.setStyleSheet('color:black')
+
+        self.lbl_desc_4 = QtWidgets.QLabel("Veuillez choisir le dossier dans lequel sera installée la base de \ndonnées. Si vous souhaitez accéder à cette base de données via un \nautre ordinateur du réseau, il est nécessaire de choisir un dossier \npartagé sur ce réseau.")
+
+        self.lbl_desc_4.setFont(QtGui.QFont("sanserif", 10))
+
+        self.btn_continue_4 = QtWidgets.QPushButton("Suivant  >", self)
+        self.btn_back_4 = QtWidgets.QPushButton("< Retour", self)
+        self.btn_browse_4 = QtWidgets.QPushButton("Parcourir", self)
+        self.le_browse_4 = QtWidgets.QLineEdit("", self)
+
+        self.btn_continue_4.setStyleSheet("background-color: rgb(150, 150, 255)")
+        self.btn_back_4.setStyleSheet("background-color: rgb(150, 150, 255)")
+        self.btn_browse_4.setStyleSheet("background-color: rgb(150, 150, 255)")
+        self.le_browse_4.setStyleSheet("background-color: rgb(255, 255, 255)")
+        self.le_browse_4.setReadOnly(True)
+
+        self.lbl_rex_pic = QtWidgets.QLabel(self)
+        pixmap = QPixmap(REX_FILE)
+        self.lbl_rex_pic.setPixmap(pixmap)
+
+        self.grid_layout.addWidget(self.lbl_title, 0, 1, 1, 4)
+        self.grid_layout.addWidget(self.lbl_desc_4, 1, 1, 1, 4)
+
+        self.grid_layout.addWidget(self.le_browse_4, 2, 1, 1, 3)
+        self.grid_layout.addWidget(self.btn_browse_4, 2, 4, 1, 1)
+
+        self.grid_layout.addWidget(self.lbl_rex_pic, 0, 0, 6, 1)
+        self.grid_layout.addWidget(self.btn_continue_4, 5, 4, 1, 1)
+        self.grid_layout.addWidget(self.btn_back_4, 5, 3, 1, 1)
+
+    def setup_default(self):
+        global DISC
+        global default_path
+        DISC = CUR_DIR.split(":/")[0]
+        with open(VERS_FILE, "r", encoding='utf8') as f :
+            data = json.load(f)
+        f.close()
+        vers = data["version"]
+        default_path = f"{DISC.capitalize()}:/{vers}/database"
+        self.le_browse_4.setText(default_path)
+
+    def setup_connections(self):
+        self.btn_continue_4.clicked.connect(self.window5)
+        self.btn_back_4.clicked.connect(self.window3)
+        self.btn_browse_4.clicked.connect(self.browser)
+
+    def browser(self):
+        shared_path = QFileDialog.getExistingDirectory(self, "Sélectionner un dossier d'installation :")
+        if shared_path != False :
+            self.le_browse_4.setText(shared_path)
+
+    def window3(self):
+        self.w = Window3()
+        self.w.show()
+        self.close()
+
+    def window5(self):
+        with open(SHARED_PATH_FILE, "w", encoding='utf8') as f :
+            vers = {"shared_directory" : self.le_browse_4.text()}
+            json.dump(vers, f, ensure_ascii=False)
+        f.close()
+        self.w = Window5()
+        self.w.show()
+        self.close()
+
+class Window5(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("MATOSAURUS SUITE INSTALLER - Alpha")
+        self.setStyleSheet("background-color: rgb(215, 215, 255)")
+        self.setMinimumWidth(700)
+        self.setMinimumHeight(508)
+        self.setMaximumWidth(700)
+        self.setMaximumHeight(508)
+        self.setWindowIcon(QtGui.QIcon(ICON_FILE))
+        
+        self.windowUI()
+        self.setup_default()
+        self.setup_connections()
+        self.show()
+
+    def windowUI(self):
+        self.grid_layout = QtWidgets.QGridLayout(self)
+
+        self.lbl_title = QtWidgets.QLabel("Récapitulatif de l'installation.")
+        self.lbl_title.setFont(QtGui.QFont("sanserif", 18))
+        self.lbl_title.setStyleSheet('color:black')
+
+        self.lbl_desc_5 = QtWidgets.QLabel("Veuillez vérifier les paramètres d'installation.")
+        self.lbl_desc_5.setFont(QtGui.QFont("sanserif", 10))
+
+        self.te_param_5 = QtWidgets.QTextEdit("")
+        self.te_param_5.setReadOnly(True)
+
+        self.btn_install = QtWidgets.QPushButton("Installer", self)
+        self.btn_back_5 = QtWidgets.QPushButton("< Retour", self)
+
+        self.btn_install.setStyleSheet("background-color: rgb(150, 150, 255)")
+        self.btn_back_5.setStyleSheet("background-color: rgb(150, 150, 255)")
+
+        self.lbl_rex_pic = QtWidgets.QLabel(self)
+        pixmap = QPixmap(REX_FILE)
+        self.lbl_rex_pic.setPixmap(pixmap)
+
+        self.grid_layout.addWidget(self.lbl_title, 0, 1, 1, 4)
+        self.grid_layout.addWidget(self.lbl_desc_5, 1, 1, 1, 4)
+
+        self.grid_layout.addWidget(self.te_param_5, 2, 1, 2, 4)
+
+        self.grid_layout.addWidget(self.lbl_rex_pic, 0, 0, 6, 1)
+        self.grid_layout.addWidget(self.btn_install, 5, 4, 1, 1)
+        self.grid_layout.addWidget(self.btn_back_5, 5, 3, 1, 1)
+
+        self.te_param_5.setStyleSheet("background-color: rgb(255, 255, 255)")
+
+        self.grid_layout.setRowStretch(0, 1)
+        self.grid_layout.setRowStretch(1, 1)
+        self.grid_layout.setRowStretch(2, 2)
+        self.grid_layout.setRowStretch(3, 2)
+        self.grid_layout.setRowStretch(4, 2)
+        self.grid_layout.setRowStretch(5, 1)
+
+    def setup_default(self):
+        with open(VERS_FILE, "r", encoding='utf8') as f :
+            data = json.load(f)
+        f.close()
+        vers = data["version"]
+        with open(PATH_FILE, "r", encoding='utf8') as f :
+            data = json.load(f)
+        f.close()
+        directory = data["directory"]
+        with open(SHARED_PATH_FILE, "r", encoding='utf8') as f :
+            data = json.load(f)
+        f.close()
+        sh_directory = data["shared_directory"]
+
+        self.te_param_5.setText(f"Paramètres d'installation :\n\n------------------------------------------------------------------------------\n\n - Version : {vers}.\n\n - Dossier d'installation : \n    {directory}.\n\n - Chemin d'accès à la base de données partagée : \n    {sh_directory}.")
+
+    def setup_connections(self):
+        self.btn_install.clicked.connect(self.window6)
+        self.btn_back_5.clicked.connect(self.window4)
+
+    def window4(self):
+        self.w = Window4()
+        self.w.show()
+        self.close()
+
+    def window6(self):
+        pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
